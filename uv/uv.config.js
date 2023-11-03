4a97607fdbade6ef691d7315f271690a2740423e
ChessAITest <147644008+ChessAITest@users.noreply.github.com>
2023-11-03 09:49:58 -0400

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -2,7 +2,7 @@
 
 self.__uv$config = {
   prefix: "/hyperviolet-us-server/uv/service/",
-  bare: "https://8vk2d6-6987.csb.app/",
+  bare: "https://zgl3jk-8080.csb.app/outerspace/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/hyperviolet/uv/uv.handler.js",
